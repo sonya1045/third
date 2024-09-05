b89f333b9aaf94fed42d57be8a1cd3cc9ad2b372
sonya1045 <smikolaenko7@gmail.com>
2024-09-05 12:46:40 +0300

    update

---
+++ myapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    published_date =  models.DateTimeField(auto_now_add=True)
